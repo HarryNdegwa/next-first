b9566b75d35b490d3454eadc5d6ed89aef8c5112
HarryNdegwa <harrisonndegwa65@gmail.com>
2020-10-05 10:44:07 +0300

    Added head to first post page

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,8 +1,12 @@
 import Link from "next/link";
+import Head from "next/head";
 
 const firstPost = () => {
   return (
     <div>
+      <Head>
+        <title>First Post</title>
+      </Head>
       <h1>First Post</h1>
       <a className="my-link" href="https://google.com" target="_blank">
         Visit Google
