729f5b1cfcbd6ccc96025079fc96b453ea1b90d7
HarryNdegwa <harrisonndegwa65@gmail.com>
2020-10-05 10:58:26 +0300

    Added layout module css

--- components/layout.js
+++ components/layout.js
@@ -1,5 +1,7 @@
+import styles from "./layout.module.css";
+
 const layout = ({ children }) => {
-  return <div>{children}</div>;
+  return <div className={styles.container}>{children}</div>;
 };
 
 export default layout;
--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,19 +1,23 @@
 import Link from "next/link";
 import Head from "next/head";
 
+import Layout from "../../components/layout";
+
 const firstPost = () => {
   return (
     <div>
-      <Head>
-        <title>First Post</title>
-      </Head>
-      <h1>First Post</h1>
-      <a className="my-link" href="https://google.com" target="_blank">
-        Visit Google
-      </a>
-      <Link href="/">
-        <a className="my-link">Back to home</a>
-      </Link>
+      <Layout>
+        <Head>
+          <title>First Post</title>
+        </Head>
+        <h1>First Post</h1>
+        <a className="my-link" href="https://google.com" target="_blank">
+          Visit Google
+        </a>
+        <Link href="/">
+          <a className="my-link">Back to home</a>
+        </Link>
+      </Layout>
     </div>
   );
 };
