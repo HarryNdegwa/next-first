7ad66d38d2ea169b6d37e82212678c865fa49e05
HarryNdegwa <harrisonndegwa65@gmail.com>
2020-10-05 13:25:06 +0300

    Added dynamic posts fetch logic

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,16 +1,34 @@
 import Layout from "../../components/layout";
+import { getAllPostIds, getPostData } from "../../lib/posts";
 
-const Post = () => {
-  return <Layout></Layout>;
+const Post = ({ postData }) => {
+  return (
+    <Layout>
+      {postData.title}
+      <br />
+      {postData.id}
+      <br />
+      {postData.date}
+    </Layout>
+  );
 };
 
 export default Post;
 
 export async function getStaticPaths() {
+  const paths = getAllPostIds();
   // return an array with possible values of id
+  return {
+    paths,
+    fallback: false,
+  };
 }
 
-export async function getStaticProps() {
+export async function getStaticProps({ params }) {
   //called on build in production but in development it is called in every request
   //fetches necessary data for the posts with id
+  const postData = getPostData(params.id);
+  return {
+    props: postData,
+  };
 }
